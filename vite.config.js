fefbbeeb570311e8ba13b2f1ce5feefbd65526e7
Daniel Orejuela <danielorejuelag@gmail.com>
2026-07-11 20:33:44 -0500

    Cambio en de puerto para el frontend por conflictos

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 3000,
+    strictPort: true,
+    // Necesario para que Docker en Windows pueda mapear el puerto correctamente
+    allowedHosts: 'all',
   },
 })
